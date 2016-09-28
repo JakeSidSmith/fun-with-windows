a9adf45b1cf43fa9b25ccfba406b516d505417fc
Jake 'Sid' Smith <jake@dabapps.com>
2016-09-28 16:33:34 +0100

    The fun begins

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+(function () {
+
+  var win;
+  var width = 150;
+  var height = 100;
+  var velX, velY;
+
+  function openWindow () {
+    win = window.open(
+      '',
+      '_blank',
+      'width=' + width + ',' +
+      'height=' + height + ',' +
+      'left=' + (screen.width / 2 - width / 2) + ',' +
+      'top=' + (screen.height / 2 - height / 2)
+    );
+
+    // window.win = win;
+  }
+
+  function init () {
+    var xSpeed = Math.random() * 20;
+    var ySpeed = Math.random() * (20 - xSpeed);
+
+    velX = Math.random() >= 0.5 ? xSpeed : -xSpeed;
+    velY = Math.random() >= 0.5 ? ySpeed : -ySpeed;
+    openWindow();
+  }
+
+  setInterval(function () {
+    if (win && !win.closed) {
+
+      if (win.screenX + velX <= 50 || win.screenX + velX + width >= screen.width - 50) {
+        velX *= -1;
+      } else if (win.screenY + velY <= 50 || win.screenY + velY + height >= screen.height - 50) {
+        velY *= -1;
+      }
+
+      win.moveBy(velX, velY);
+
+      velX *= 1.01;
+      velY *= 1.01;
+    } else {
+      init();
+    }
+  }, 1000 / 60);
+
+})();
